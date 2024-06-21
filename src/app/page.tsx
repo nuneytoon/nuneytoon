7ed2f4df6c336f82082ae98ae14a4164c442721a
Matt Nunemacher <mnunemacher@gmail.com>
2024-06-21 12:13:09 -0600

    WIP - modify welcome screen to have fun animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
+import Image from 'next/image'
 import Link from 'next/link'
+import bugsBunny from '../../public/bugs-bunny.svg'
 
 export default function Welcome() {
   return (
-    <main className="welcome flex min-h-screen flex-col items-center justify-center p-24">
-      <Link href="/home" className="text-amber-500">
-        Explore
-      </Link>
-    </main>
+    // <main className="welcome flex min-h-screen flex-col items-center justify-center p-24">
+    //   <Image src={bugsBunny} alt="bugs bunny" height={800} width={500} />
+    //   <Link href="/home" className="text-amber-500">
+    //     Explore
+    //   </Link>
+    // </main>
+    <div id="welcome-container">
+      <div className="introduction"></div>
+    </div>
   )
 }
